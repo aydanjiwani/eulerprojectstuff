7a2402d4e72c47c5f0e5e283165d0884c89496b4
helpmynamewontfi <aydan.jiwani@gmail.com>
2017-07-17 17:34:00 -0400

    Update Eulerproject6.cpp

--- Eulerproject6.cpp
+++ Eulerproject6.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// main() is where program execution begins.
+// Problem: Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 int main() {
 int x = 0;
